1db760c03c88a8ec0d98cb73821ca9ad7c868779
Jonathan Mutakura <jonathan@jonathans-mbp.gateway.sasktel.net>
2019-08-16 17:18:41 -0600

    connected to mongoDB

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 //DB Config
 const db = require('./config/keys').MongoURI;
 
+//Connect to Mongo
+mongoose.connect(db, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
