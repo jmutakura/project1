5501b2af674ae8b8740b1afb1dccbcb80b797047
Jonathan Mutakura <jonathan@jonathans-mbp.gateway.sasktel.net>
2019-08-16 17:03:06 -0600

    First commit including the final ejs files and linked to MongoDB Atlas

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys').MongoURI;
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://jmutakura:Peacebestill101@cluster0-9918z.mongodb.net/test?retryWrites=true&w=majority'
+}
